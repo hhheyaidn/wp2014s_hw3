b1f3429256f10150390e0c6b04e82038bbb7dfab
hhheyaidn <hhheyaidn26@gmail.com>
2014-05-28 15:28:26 +0800

    v2.0

--- js/app.js
+++ js/app.js
@@ -4,28 +4,25 @@
   //編譯template engine函數();
   
   var templates = {};
-  ['loginView', 'evaluationView', 'updateSuccessView'].forEach(function(e){
-    var tpl = document.getElementById(e).text;
-    templates[e] = doT.template(tpl);
+  ["loginView", "evaluationView", "updateSuccessView"].forEach(function (e) {
+    templateCode = document.getElementById(e).text;
+    templates[e] = doT.template(templateCode);
   });
 
-  //可選-編寫共用函數();
-  
-  var commons={
-	loginRequiredView:function(aaa){
-		return function(){
-				var current=Parse.User.current();
-				if(current){
-					aaa();
-				}
-				else{
-					window.location.hash="login/"+window.location.hash;
-				}
-			}
-		}
-	};
-  
-  var handler = {
+  var commons = {
+    loginRequiredView: function (ViewFunction) {
+      return function () {
+        var currentUser = Parse.User.current();
+        if (currentUser) {
+          ViewFunction();
+        } else {
+          window.location.hash = "login/" + window.location.hash;
+        }
+      }
+    },
+  }
+
+  var handlers = {
     navbar: function(){
 	  var currentUser = Parse.User.current();
       if(currentUser){
@@ -45,12 +42,61 @@
 		window.location.hash="login/"
 		})
     },
-    loginView: function(redirect){
-	  document.getElementById('content').innerHTML = templates.loginView();
-	  
-     // 綁定登入表單的學號檢查事件(); // 可以利用TAHelp物件
-     // 綁定註冊表單的學號檢查事件(); // 可以利用TAHelp物件
-	 
+    evaluationView: commons.loginRequiredView(function () {
+      var Evaluation = Parse.Object.extend('Evaluation');
+      var currentUser = Parse.User.current();      
+      var evaluationACL = new Parse.ACL();
+      evaluationACL.setPublicReadAccess(false);
+      evaluationACL.setPublicWriteAccess(false);
+      evaluationACL.setReadAccess(currentUser, true);
+      evaluationACL.setWriteAccess(currentUser, true);
+      var query = new Parse.Query(Evaluation);
+      query.equalTo('user', currentUser);
+      query.first({
+        success: function(evaluation){
+          window.EVAL = evaluation;
+          if(evaluation === undefined){
+            var TeamMembers = TAHelp.getMemberlistOf(currentUser.get('username')).filter(function(e){
+              return (e.StudentId !== currentUser.get('username') ) ? true : false;
+            }).map(function(e){
+              e.scores = ['0', '0', '0', '0'];
+              return e;
+            });
+          } else {
+            var TeamMembers = evaluation.toJSON().evaluations;
+          }
+          document.getElementById('content').innerHTML = templates.evaluationView(TeamMembers);
+          document.getElementById('evaluationForm-submit').value = ( evaluation === undefined ) ? '送出表單' :'修改表單';
+          document.getElementById('evaluationForm').addEventListener('submit', function(){
+            for(var i = 0; i < TeamMembers.length; i++){
+              for(var j = 0; j < TeamMembers[i].scores.length; j++){
+                var e = document.getElementById('stu'+TeamMembers[i].StudentId+'-q'+j);
+                var amount = e.options[e.selectedIndex].value;
+                TeamMembers[i].scores[j] = amount;
+              }
+            }
+            if( evaluation === undefined ){
+              evaluation = new Evaluation();
+              evaluation.set('user', currentUser);
+              evaluation.setACL(evaluationACL);
+            }
+            console.log(TeamMembers);
+            evaluation.set('evaluations', TeamMembers);
+            evaluation.save(null, {
+              success: function(){
+                document.getElementById('content').innerHTML = templates.updateSuccessView();
+              },
+              error: function(){},
+            });
+
+          }, false);
+        }, error: function(object, err){
+        
+        }
+      }); 
+    }),
+    loginView: function (redirect) {
+	
 	  var message = '此學號不在修課名單內，請重試。';
 	  document.getElementById('form-signin-message').innerHTML = message;
 	  document.getElementById('form-signup-message').innerHTML = message;
@@ -70,131 +116,89 @@
 		 else
 			document.getElementById('form-signup-message').style.display="none";
         });
-	 
-     // 綁定註冊表單的密碼檢查事件(); // 參考上課範例
-     // 綁定登入表單的登入檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.logIn
-     // 綁定註冊表單的註冊檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.signUp和相關函數
-	  var currentUser = Parse.User.current();
-      // What to do after signin / signup is successfully performed.
-      var postAction = function(){
-        handler.navbar();
-        window.location.hash = (redirect)? redirect :''
+	
+     
+      var postAction = function() {
+        handlers.navbar();
+        window.location.hash = (redirect) ? redirect : '';
       }
-      
-      if (currentUser) {
-        window.location.hash = '';
-      } else {
-        // Signin Function binding, provided by Parse SDK.        
-        document.getElementById('form-signin').addEventListener('submit', function(){
-          Parse.User.logIn(document.getElementById('form-signin-student-id').value,
-              document.getElementById('form-signin-password').value, {
-            success: function(user) {
-              // Do stuff after successful login.
-              postAction();
-            },
-            error: function(user, error) {
-              // The login failed. Check error to see why.
-            }
-          }); 
-        }, false);
-        // Signup Form Password Match Check Binding.
-        document.getElementById('form-signup-password1').addEventListener('keyup', function(){
-          var singupForm_password = document.getElementById('form-signup-password');
-          var message = (this.value !== singupForm_password.value) ? '密碼不一致，請再確認一次。' : '';
-          document.getElementById('form-signup-message').innerHTML = message;
-		  if(this.value !== singupForm_password.value)
-			document.getElementById('form-signup-message').style.display="block";
-          else
-			document.getElementById('form-signup-message').style.display="none";
-		});
-        // Signup Function binding, provided by Parse SDK.
-        document.getElementById('form-signup').addEventListener('submit', function(){
-          var user = new Parse.User();
-          user.set("username", document.getElementById('form-signup-student-id').value);
-          user.set("password", document.getElementById('form-signup-password').value);
-          user.set("email", document.getElementById('form-signup-email').value);
- 
-          user.signUp(null, {
+      var passwordMatch = function(){
+        var singupForm_password = document.getElementById('form-signup-password');
+        var singupForm_password1 = document.getElementById('form-signup-password1');
+        var BOOL = (singupForm_password.value === singupForm_password1.value) ? true : false;
+        showMessage('form-signup-message', function(){return BOOL;}, 'Passwords don\'t match.');
+        return BOOL;
+      }
+
+      document.getElementById("content").innerHTML = templates.loginView();
+      document.getElementById("form-signin-student-id").addEventListener("keyup", function () {
+        showMessage('form-signin-message', function(){return checkVaildStudentID("form-signin-student-id")}
+            , 'The student is not one of the class students.');
+      });
+      document.getElementById("form-signin").addEventListener("submit", function () {
+        if (!checkVaildStudentID("form-signin-student-id")) {
+          alert("The student is not one of the class students.");
+          return false;
+        }
+        Parse.User.logIn(document.getElementById("form-signin-student-id").value,
+          document.getElementById("form-signin-password").value, {
             success: function(user) {
               postAction();
-              // Hooray! Let them use the app now.
             },
-            error: function(user, error) {
-              // Show the error message somewhere and let the user try again.
-              document.getElementById('form-signup-message').innerHTML = error.message + '['+error.code+']';
+            error: function (user, error) {
+              showMessage('form-signin-message', function () {
+                return false;
+              }, "Invaild username or password.");
             }
           });
-        }, false);
-      } 
-	 
-    },
-	evaluationView:commons.loginRequiredView(function(){
-			var t=Parse.Object.extend("Evaluation");
-			var n=Parse.User.current();
-			var r=new Parse.ACL;
-			r.setPublicReadAccess(false);
-			r.setPublicWriteAccess(false);
-			r.setReadAccess(n,true);
-			r.setWriteAccess(n,true);
-			var i=new Parse.Query(t);
-			i.equalTo("user",n);
-			i.first({
-				success:function(i){
-					window.EVAL=i;
-					if(i===undefined){
-						var s=TAHelp.getMemberlistOf(n.get("username")).filter(function(e){
-							return e.StudentId!==n.get("username")?true:false
-						}).map(function(e){
-							e.scores=["0","0","0","0"];
-							return e
-						})
-					}
-					else{
-						var s=i.toJSON().evaluations
-					}
-					document.getElementById("content").innerHTML=e.evaluationView(s);
-					document.getElementById("evaluationForm-submit").value=i===undefined?"�銵典":"靽格銵典";
-					document.getElementById("evaluationForm").addEventListener("submit",function(){
-						for(var o=0;o<s.length;o++){
-							for(var u=0;u<s[o].scores.length;u++){
-								var a=document.getElementById("stu"+s[o].StudentId+"-q"+u);
-								var f=a.options[a.selectedIndex].value;s[o].scores[u]=f
-							}
-						}
-						if(i===undefined){
-							i=new t;
-							i.set("user",n);
-							i.setACL(r)}console.log(s);
-							i.set("evaluations",s);
-							i.save(null,{
-								success:function(){
-									document.getElementById("content").innerHTML=e.updateSuccessView()
-								},
-								error:function(){
+      }, false);
 
-								}
-							})
-					},false);
-				},
-				error:function(e,t){
+      document.getElementById("form-signup-student-id").addEventListener("keyup", function () {
+        showMessage('form-signup-message', function(){return checkVaildStudentID("form-signup-student-id")}
+            , 'The student is not one of the class students.');
+      });
+      document.getElementById("form-signup-password1").addEventListener('keyup', passwordMatch);
+      document.getElementById("form-signup").addEventListener("submit", function (){
+        if (!checkVaildStudentID('form-signup-student-id')){
+          alert("The student is not one of the class students."); 
+          return false;
+        }
+        var BOOL = passwordMatch();
+        if(!BOOL){
+          return false;
+        }
 
-				}
-			})
-		}),
-	
+        var user = new Parse.User();
+        user.set("username", document.getElementById('form-signup-student-id').value);
+        user.set("password", document.getElementById('form-signup-password').value);
+        user.set("email", document.getElementById('form-signup-email').value);
+        user.signUp(null, {
+          success: function(user){
+            postAction();
+          },
+          error: function(user, error){
+            showMessage('form-signup-message', function () {
+              return false;
+            }, error.message);
+          }
+        });
+      }, false);
+    }
   };
-	
-	var r = Parse.Router.extend({
-		routes: {
-			'': 'index',
-			'peer-evaluation': 'evaluation',
-			'login/*redirect': 'login',
-		},
-		index: handler.evaluationView,
-		login: handler.loginView,
-		evaluation: handler.evaluationView,
-	});
-	this.Router = new r();
-	Parse.history.start();
-	handler.navbar();
+
+  var Router = Parse.Router.extend({
+    routes: {
+      "": "indexView",
+      "peer-evaluation/": "evaluationView",
+      "login/*redirect": "loginView",
+    },
+    indexView: handlers.evaluationView,
+    evaluationView: handlers.evaluationView,
+    loginView: handlers.loginView,
+  });
+
+  this.Router = new Router();
+  Parse.history.start();
+  handlers.navbar();
+
 })();
