eb6ca878ca0165071b2704f9199c049aa795bc60
hhheyaidn <hhheyaidn26@gmail.com>
2014-05-28 14:31:33 +0800

    v2.0

--- js/app.js
+++ js/app.js
@@ -2,44 +2,135 @@
   Parse.initialize("265RdtKjvGmVHSwHQXDXv2XXw7aGP5BfG175jjha",
       "RNHeGkUvVEc8DUMJCbtbpCPUOdxWbtuKB7TkhChZ");
 	  
-  var e={}; // e--->template
-	["loginView","evaluationView","updateSuccessView"].forEach(function(t){
-		templateCode=document.getElementById(t).text;
-		e[t]=doT.template(templateCode)
-	});
-	var t={
-		loginRequiredView:function(e){
-			return function(){
-				var t=Parse.User.current();
-				if(t){
-					e()
-				}
-				else{
-					window.location.hash="login/"+window.location.hash
-				}
-			}
-		}
-	};
-	var n={ //n----->handler
-		navbar:function(){
-			var e=Parse.User.current();
-			if(e){
-				document.getElementById("loginButton").style.display="none";
-				document.getElementById("logoutButton").style.display="block";
-				document.getElementById("evaluationButton").style.display="block"
+  //編譯template engine函數();
+  
+  var templates = {};
+  ['loginView', 'evaluationView', 'updateSuccessView'].forEach(function(e){
+    var tpl = document.getElementById(e).text;
+    templates[e] = doT.template(tpl);
+  });
+
+  //可選-編寫共用函數();
+  
+  var t={
+	loginRequiredView:function(e){
+		return function(){
+			var current=Parse.User.current();
+			if(current){
+				e()
 			}
 			else{
-				document.getElementById("loginButton").style.display="block";
-				document.getElementById("logoutButton").style.display="none";
-				document.getElementById("evaluationButton").style.display="none"
+				window.location.hash="login/"+window.location.hash
 			}
-			document.getElementById("logoutButton").addEventListener("click",function(){
-				Parse.User.logOut();
-				n.navbar();
-				window.location.hash="login/"
-			})
-		},
-		evaluationView:t.loginRequiredView(function(){
+		}
+	}
+	};
+  
+  var handler = {
+    navbar: function(){
+	  var currentUser = Parse.User.current();
+      if(currentUser){
+        //顯示哪些button();
+		document.getElementById("loginButton").style.display="none";
+		document.getElementById("evaluationButton").style.display="block";
+	    document.getElementById("logoutButton").style.display="block";
+      } else {
+       // 顯示哪些button();      
+	    document.getElementById("loginButton").style.display="block";
+	    document.getElementById("evaluationButton").style.display="none";
+	    document.getElementById("logoutButton").style.display="none";
+      }
+	  document.getElementById("logoutButton").addEventListener('click',function(){
+		Parse.User.logOut();
+		handler.navbar();
+		window.location.hash="login/"
+		})
+    },
+    loginView: function(redirect){
+	  document.getElementById('content').innerHTML = templates.loginView();
+	  
+     // 綁定登入表單的學號檢查事件(); // 可以利用TAHelp物件
+     // 綁定註冊表單的學號檢查事件(); // 可以利用TAHelp物件
+	 
+	  var message = '此學號不在修課名單內，請重試。';
+	  document.getElementById('form-signin-message').innerHTML = message;
+	  document.getElementById('form-signup-message').innerHTML = message;
+	  
+	  document.getElementById('form-signin-student-id').addEventListener('keyup', function(){
+		if(TAHelp.getMemberlistOf(this.value)==false){	  
+			document.getElementById('form-signin-message').style.display="block";
+		  }
+		 else
+			document.getElementById('form-signin-message').style.display="none";
+			});
+		
+	  document.getElementById('form-signup-student-id').addEventListener('keyup', function(){
+		if(TAHelp.getMemberlistOf(this.value)==false){	  
+			document.getElementById('form-signup-message').style.display="block";
+		  }
+		 else
+			document.getElementById('form-signup-message').style.display="none";
+        });
+	 
+     // 綁定註冊表單的密碼檢查事件(); // 參考上課範例
+     // 綁定登入表單的登入檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.logIn
+     // 綁定註冊表單的註冊檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.signUp和相關函數
+	  var currentUser = Parse.User.current();
+      // What to do after signin / signup is successfully performed.
+      var postAction = function(){
+        handler.navbar();
+        window.location.hash = t ? t : '';
+      }
+      
+      if (currentUser) {
+        window.location.hash = '';
+      } else {
+        // Signin Function binding, provided by Parse SDK.        
+        document.getElementById('form-signin').addEventListener('submit', function(){
+          Parse.User.logIn(document.getElementById('form-signin-student-id').value,
+              document.getElementById('form-signin-password').value, {
+            success: function(templates) {
+              // Do stuff after successful login.
+              postAction();
+			  console.log(user);
+            },
+            error: function(user, error) {
+              // The login failed. Check error to see why.
+            }
+          }); 
+        });
+        // Signup Form Password Match Check Binding.
+        document.getElementById('form-signup-password1').addEventListener('keyup', function(){
+          var singupForm_password = document.getElementById('form-signup-password');
+          var message = (this.value !== singupForm_password.value) ? '密碼不一致，請再確認一次。' : '';
+          document.getElementById('form-signup-message').innerHTML = message;
+		  if(this.value !== singupForm_password.value)
+			document.getElementById('form-signup-message').style.display="block";
+          else
+			document.getElementById('form-signup-message').style.display="none";
+		});
+        // Signup Function binding, provided by Parse SDK.
+        document.getElementById('form-signup').addEventListener('submit', function(){
+          var user = new Parse.User();
+          user.set("username", document.getElementById('form-signup-student-id').value);
+          user.set("password", document.getElementById('form-signup-password').value);
+          user.set("email", document.getElementById('form-signup-email').value);
+ 
+          user.signUp(null, {
+            success: function(user) {
+              postAction();
+              // Hooray! Let them use the app now.
+            },
+            error: function(user, error) {
+              // Show the error message somewhere and let the user try again.
+              document.getElementById('form-signup-message').innerHTML = error.message + '['+error.code+']';
+            }
+          });
+        }, false);
+      } 
+	 
+    },
+	evaluationView:t.loginRequiredView(function(){
 			var t=Parse.Object.extend("Evaluation");
 			var n=Parse.User.current();
 			var r=new Parse.ACL;
@@ -64,7 +155,7 @@
 						var s=i.toJSON().evaluations
 					}
 					document.getElementById("content").innerHTML=e.evaluationView(s);
-					document.getElementById("evaluationForm-submit").value=i===undefined?"�銵典":"靽格銵典";
+					document.getElementById("evaluationForm-submit").value=i===undefined?"提交表單":"修改表單";
 					document.getElementById("evaluationForm").addEventListener("submit",function(){
 						for(var o=0;o<s.length;o++){
 							for(var u=0;u<s[o].scores.length;u++){
@@ -92,91 +183,19 @@
 				}
 			})
 		}),
-		loginView:function(t){
-	var r=function(e){
-		var t=document.getElementById(e).value;
-		return TAHelp.getMemberlistOf(t)===false?false:true
-	};
-	var i=function(e,t,n){
-		if(!t()){
-			document.getElementById(e).innerHTML=n;
-			document.getElementById(e).style.display="block"
-		}
-		else{
-			document.getElementById(e).style.display="none"
-		}
-	};
-	var s=function(){
-		n.navbar();
-		window.location.hash=t?t:""
-	};
-	var o=function(){
-		var e=document.getElementById("form-signup-password");
-		var t=document.getElementById("form-signup-password1");
-		var n=e.value===t.value?true:false;
-		i("form-signup-message",function(){return n},"Passwords don't match.");
-		return n
-	};
-	document.getElementById("content").innerHTML=e.loginView();
-	document.getElementById("form-signin-student-id").addEventListener("keyup",function(){
-		i("form-signin-message",function(){
-			return r("form-signin-student-id")
-		},"The student is not one of the class students.")
-	});
-	document.getElementById("form-signin").addEventListener("submit",function(){
-		if(!r("form-signin-student-id")){
-			alert("The student is not one of the class students.");
-			return false
-		}
-		Parse.User.logIn(document.getElementById("form-signin-student-id").value,document.getElementById("form-signin-password").value,{
-			success:function(e){
-				s()
-			},
-			error:function(e,t){
-				i("form-signin-message",function(){return false},"Invaild username or password.")
-			}
-		})
-	},false);
-	document.getElementById("form-signup-student-id").addEventListener("keyup",function(){
-		i("form-signup-message",function(){return r("form-signup-student-id")},"The student is not one of the class students.")
-	});
-	document.getElementById("form-signup-password1").addEventListener("keyup",o);
-	document.getElementById("form-signup").addEventListener("submit",function(){
-		if(!r("form-signup-student-id")){
-			alert("The student is not one of the class students.");
-			return false
-		}
-
-		var e=o();
-		if(!e){
-			return false
-		}
-		var t=new Parse.User;
-		t.set("username",document.getElementById("form-signup-student-id").value);
-		t.set("password",document.getElementById("form-signup-password").value);
-		t.set("email",document.getElementById("form-signup-email").value);
-		t.signUp(null,{
-			success:function(e){
-				s()
-			},
-			error:function(e,t){
-				i("form-signup-message",function(){return false},t.message)
-			}
-		})
-	},false)
-	}};
-	var r=Parse.Router.extend({
-		routes:{
-			"":"indexView",
-			"peer-evaluation/":"evaluationView",
-			"login/*redirect":"loginView"
+  };
+	
+	var r = Parse.Router.extend({
+		routes: {
+			'': 'index',
+			'peer-evaluation': 'evaluation',
+			'login/*redirect': 'login',
 		},
-		indexView:n.evaluationView,
-		evaluationView:n.evaluationView,
-		loginView:n.loginView
+		index: handler.loginView,
+		login: handler.loginView,
+		evaluation: handler.evaluationView,
 	});
-	this.Router=new r;
+	this.Router = new r();
 	Parse.history.start();
-	n.navbar()
-})
-()
+	handler.navbar();
+})();
